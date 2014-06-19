caba95eb19d6c6faa972f5c92c8ecb935072f43f
Alexandre Leray <alexandre@stdin.fr>
2014-06-19 17:36:19 +0200

    Exposes various time-related utilities

--- src/dialects/aa.js
+++ src/dialects/aa.js
@@ -42,6 +42,8 @@ define(['../markdown_helpers', './dialect_helpers', './maruku', '../parser'], fu
     mm = _[0];
     ss = _[1];
 
+    ms = parseInt(ms, 10);
+
     ms = rpad(ms, 3);
     ss = lpad(ss, 2);
     mm = lpad(mm, 2);
@@ -305,6 +307,16 @@ define(['../markdown_helpers', './dialect_helpers', './maruku', '../parser'], fu
     // Just consume the '[['
     return [ 2, "%%" ];
   };
+
+
+  // Exposes the various utils because they might be useful elsewhere
+  Aa.utils = Aa.utils || {};
+  Aa.utils.rpad = rpad;
+  Aa.utils.lpad = lpad;
+  Aa.utils.divmod = divmod;
+  Aa.utils.ms2tc = ms2tc;
+  Aa.utils.ss2tc = ss2tc;
+  Aa.utils.tc2ss = tc2ss;
 
 
   Markdown.dialects.Aa = Aa;
